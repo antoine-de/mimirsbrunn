1670e2c28c16bf511ce2762adf39c4f2f9bf08fb
antoine-de <antoine.desbordes@gmail.com>
2016-04-01 12:35:29 +0200

    first bano import works

--- src/bin/bano2mimir.rs
+++ src/bin/bano2mimir.rs
@@ -22,8 +22,55 @@ extern crate mimirsbrunn;
 
 use std::path::Path;
 
-pub fn index<I: Iterator<Item = mimirsbrunn::Addr>>(iter: I) -> Result<u32, curl::ErrCode> {
-    unreachable!()
+
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct Bano {
+    pub id: String,
+    pub nb: String,
+    pub street: String,
+    pub zip: String,
+    pub city: String,
+    pub src: String,
+    pub lat: f64,
+    pub lon: f64,
+}
+
+impl Bano {
+    pub fn insee(&self) -> &str {
+        assert!(self.id.len() >= 5);
+        &self.id[..5]
+    }
+    pub fn fantoir(&self) -> &str {
+        assert!(self.id.len() >= 10);
+        &self.id[..10]
+    }
+    pub fn into_addr(self) -> mimirsbrunn::Addr {
+        let street_name = format!("{}, {} {}", self.street, self.zip, self.city);
+        let addr_name = format!("{} {}", self.nb, street_name);
+        let street_id = format!("street:{}", self.fantoir().to_string());
+        let admin = mimirsbrunn::Admin {
+            id: format!("admin:{}", self.insee()),
+            level: 8,
+            name: self.city,
+            zip_code: self.zip,
+            weight: 1,
+        };
+        let street = mimirsbrunn::Street {
+            id: street_id,
+            street_name: self.street,
+            name: street_name,
+            administrative_region: admin,
+            weight: 1,
+        };
+        mimirsbrunn::Addr {
+            id: format!("addr:{};{}", self.lat, self.lon),
+            house_number: self.nb,
+            street: street,
+            name: addr_name,
+            coord: mimirsbrunn::Coord { lat: self.lat, lon: self.lon },
+            weight: 1,
+        }
+    }
 }
 
 fn index_bano(files: &[String]) {
@@ -35,7 +82,9 @@ fn index_bano(files: &[String]) {
         println!("importing {}...", f);
         let mut rdr = csv::Reader::from_file(&Path::new(&f)).unwrap().has_headers(false);
 
-
+        let iter = rdr.decode().map(|r| { let b: Bano = r.unwrap(); b.into_addr() });
+        let nb = mimirsbrunn::index(iter).unwrap();
+        println!("importing {}: {} addresses added.", f, nb);
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,53 @@ extern crate rustc_serialize;
 extern crate curl;
 
 use rustc_serialize::json;
+use rustc_serialize::Encodable;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
+
+trait Incr: Clone {
+    fn id(&self) -> &str;
+    fn incr(&mut self);
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct Coord { pub lat: f64, pub lon: f64 }
 
 #[derive(RustcDecodable, RustcEncodable, Clone)]
+pub struct Admin {
+    pub id: String,
+    pub level: u32,
+    pub name: String,
+    pub zip_code: String,
+    pub weight: u32,
+}
+impl Incr for Admin {
+    fn id(&self) -> &str { &self.id }
+    fn incr(&mut self) { self.weight += 1; }
+}
+
+#[derive(RustcDecodable, RustcEncodable, Clone)]
+pub struct Street {
+    pub id: String,
+    pub street_name: String,
+    pub name: String,
+    pub administrative_region: Admin,
+    pub weight: u32,
+}
+impl Incr for Street {
+    fn id(&self) -> &str { &self.id }
+    fn incr(&mut self) { self.weight += 1; }
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
 pub struct Addr {
     pub id: String,
     pub house_number: String,
+    pub street: Street,
     pub name: String,
+    pub coord: Coord,
     pub weight: u32,
 }
-
 
 pub fn purge_and_create_munin() -> Result<(), curl::ErrCode> {
     // first, we must delete with its own handle the old munin
@@ -24,4 +62,49 @@ pub fn purge_and_create_munin() -> Result<(), curl::ErrCode> {
     assert!(res.get_code() == 200, "Error adding analysis: {}", res);
 
     Ok(())
+}
+
+fn push_bulk<'a, T: Encodable>(s: &mut String, elt: &T) {
+    s.push_str("{index: {}}\n");
+    s.push_str(&json::encode(elt).unwrap());
+    s.push('\n');
+}
+fn bulk_index<'a, T, I>(url: &str, mut iter: I) -> Result<u32, curl::ErrCode>
+    where T: Encodable, I: Iterator<Item = T>
+{
+    let url = format!("{}/_bulk", url);
+    let mut handle = curl::http::handle();
+    let mut nb = 0;
+    let mut chunk = String::new();
+    loop {
+        chunk.clear();
+        let addr = match iter.next() { Some(a) => a, None => break };
+        push_bulk(&mut chunk, &addr);
+        nb += 1;
+        for addr in iter.by_ref().take(1000) {
+            push_bulk(&mut chunk, &addr);
+            nb += 1;
+        }
+        let res = try!(handle.post(&*url, &chunk).exec());
+        assert!(res.get_code() != 201, format!("result of bulk insert is not 201: {}", res));
+    }
+    Ok(nb)
+}
+
+fn upsert<T: Incr>(elt: &T, map: &mut HashMap<String, T>) {
+    match map.entry(elt.id().to_string()) {
+        Vacant(e) => { e.insert(elt.clone()); }
+        Occupied(mut e) => e.get_mut().incr()
+    }
+}
+
+pub fn index<I: Iterator<Item = Addr>>(iter: I) -> Result<u32, curl::ErrCode> {
+    let mut admins = HashMap::new();
+    let mut streets = HashMap::new();
+    try!(bulk_index("http://localhost:9200/munin/addr", iter.inspect(|addr| {
+        upsert(&addr.street.administrative_region, &mut admins);
+        upsert(&addr.street, &mut streets);
+    })));
+    try!(bulk_index("http://localhost:9200/munin/admin", admins.into_iter().map(|e| e.1)));
+    bulk_index("http://localhost:9200/munin/street", streets.into_iter().map(|e| e.1))
 }
