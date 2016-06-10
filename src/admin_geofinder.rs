8c301dc9e4aa07be4e9b83110910a167caec47da
antoine-de <antoine.desbordes@gmail.com>
2016-06-10 17:24:23 +0200

    quad tree clean up

--- src/admin_geofinder.rs
+++ src/admin_geofinder.rs
@@ -28,7 +28,6 @@
 // https://groups.google.com/d/forum/navitia
 // www.navitia.io
 use mimir::Admin;
-use ntree;
 use geo::contains::Contains;
 use geo;
 
@@ -40,10 +39,8 @@ pub struct AdminGeoFinder {
 impl AdminGeoFinder {
     pub fn new() -> AdminGeoFinder {
         AdminGeoFinder {
-            //quadtree: ntree::NTree::new(QuadTreeRegion::new(-180.0, -180.0, 360.0), 4)
             admins: vec![]
         }
-
     }
 
     pub fn add_admin(&mut self, admin: Admin) {
@@ -59,124 +56,8 @@ impl AdminGeoFinder {
             })
             .collect()
     }
-}
 
-#[derive(Clone, Debug, PartialEq)]
-struct QuadTreeRegion {
-    pub x: f64,
-    pub y: f64,
-    pub width: f64,
-}
-
-impl QuadTreeRegion {
-    pub fn new(x: f64, y: f64, wh: f64) -> QuadTreeRegion {
-        QuadTreeRegion {
-            y: y,
-            x: x,
-            width: wh,
-        }
-    }
-    pub fn contains_point(&self, x: f64, y: f64) -> bool {
-        self.y <= y && self.x <= x && (self.y + self.width) >= y &&
-        (self.x + self.width) >= x
-    }
-}
-
-impl ntree::Region<Admin> for QuadTreeRegion {
-    fn contains(&self, admin: &Admin) -> bool {
-        admin.coord.as_ref().map_or(false, |c| self.contains_point(c.x, c.y))
-    }
-
-    fn split(&self) -> Vec<QuadTreeRegion> {
-        println!("we split a square {:?}", &self);
-        let halfwidth = self.width / 2.0;
-        vec![
-            QuadTreeRegion::new(self.x, self.y, halfwidth),
-            QuadTreeRegion::new(self.x, self.y + halfwidth, halfwidth),
-            QuadTreeRegion::new(self.x + halfwidth, self.y, halfwidth),
-            QuadTreeRegion::new(self.x + halfwidth, self.y + halfwidth, halfwidth),
-         ]
-    }
-
-    fn overlaps(&self, other: &QuadTreeRegion) -> bool {
-        other.contains_point(self.x, self.y) ||
-        other.contains_point(self.x + self.width, self.y) ||
-        other.contains_point(self.x, self.y + self.width) ||
-        other.contains_point(self.x + self.width, self.y + self.width)
-    }
-}
-
-
-#[cfg(test)]
-mod tests {
-    use mimir::Admin;
-    use geo;
-    use ntree::Region;
-
-    #[test]
-    fn test_no_overlap() {
-        let a = super::QuadTreeRegion::new(10., 20., 5.);
-        let b = super::QuadTreeRegion::new(20., 10., 5.);
-
-        // a region must overlap itself
-        assert!(a.overlaps(&a));
-        assert!(b.overlaps(&b));
-
-        // but the 2 region does not overlap
-        assert!(! a.overlaps(&b));
-        assert!(! b.overlaps(&a));
-    }
-    #[test]
-    fn test_overlap_nerby() {
-        let a = super::QuadTreeRegion::new(10., 10., 5.);
-        let b = super::QuadTreeRegion::new(15., 10., 5.);
-
-        // b touches a, the regions overlap
-        assert!(a.overlaps(&b));
-        assert!(b.overlaps(&a));
-    }
-    #[test]
-    fn test_overlap() {
-        let a = super::QuadTreeRegion::new(-10., -10., 5.);
-        let b = super::QuadTreeRegion::new(-12., -12., 5.);
-
-        assert!(a.overlaps(&b));
-        assert!(b.overlaps(&a));
-    }
-
-    fn make_dummy_admin() -> Admin {
-        let p = |x, y| geo::Point(geo::Coordinate { x: x, y: y });
-        // the boundary is a big square around the centre of the admin
-        let square = geo::Polygon(geo::LineString(vec![p(41., 23.),
-                                                       p(41., 25.),
-                                                       p(43., 25.),
-                                                       p(43., 23.)]),
-                                  vec![]);
-        let boundary = geo::MultiPolygon(vec![square]);
-
-        Admin {
-            id: "admin:fr:bob".to_string(),
-            level: 8,
-            name: "bob".to_string(),
-            zip_code: "ziiip".to_string(),
-            weight: 1,
-            coord: Some(geo::Coordinate { x: 42.0, y: 24.0 }),
-            boundary: Some(boundary),
-            insee: "my_insee".to_string()
-        }
-    }
-
-    #[test]
-    fn test_one_admin() {
-        let a = make_dummy_admin();
-        let a_coord = a.coord.unwrap().clone();
-
-        let mut geo_finder = super::AdminGeoFinder::new();
-
-        geo_finder.add_admin(a);
-
-        let admins = geo_finder.get_admins_for_coord(&a_coord);
-
-        assert_eq!(admins.len(), 1);
+    pub fn get_admins_for_street(&self) -> Vec<&Admin> {
+        panic!("get_admin_for_street");
     }
 }
