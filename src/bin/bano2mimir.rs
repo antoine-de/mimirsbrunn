49528299fd5d3d5d130969dbf85afedb547286fb
antoine-de <antoine.desbordes@gmail.com>
2016-06-13 10:39:40 +0200

    remove useless coma

--- src/bin/bano2mimir.rs
+++ src/bin/bano2mimir.rs
@@ -64,7 +64,7 @@ impl Bano {
         &self.id[..10]
     }
     pub fn into_addr(self, admins: &AdminFromInsee) -> mimir::Addr {
-        let street_name = format!("{}, ({})", self.street, self.city);
+        let street_name = format!("{} ({})", self.street, self.city);
         let addr_name = format!("{} {}", self.nb, street_name);
         let street_id = format!("street:{}", self.fantoir().to_string());
         let admin = admins.get(&format!("admin:fr:{}", self.insee()));
--- tests/bragi_test.rs
+++ tests/bragi_test.rs
@@ -71,9 +71,9 @@ pub fn bragi_tests(es_wrapper: ::ElasticSearchWrapper) {
                          r#""features":[{"type":"Feature","geometry":{"coordinates":"#,
                          r#"[2.3763789999999996,48.846495],"type":"Point"},"#,
                          r#""properties":{"geocoding":{"id":"addr:2.376379;48.846495","#,
-                         r#""type":"house","label":"15 Rue Hector Malot, (Paris)","#,
-                         r#""name":"15 Rue Hector Malot, (Paris)","housenumber":"15","#,
-                         r#""street":"Rue Hector Malot, (Paris)","postcode":null,"#,
+                         r#""type":"house","label":"15 Rue Hector Malot (Paris)","#,
+                         r#""name":"15 Rue Hector Malot (Paris)","housenumber":"15","#,
+                         r#""street":"Rue Hector Malot (Paris)","postcode":null,"#,
                          r#""city":null,"administrative_regions":[]}}}]}"#);
     assert_eq!(result_body, result);
 }
