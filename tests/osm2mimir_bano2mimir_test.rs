79de26276148a5989f56b1b2f00a513847ba9e11
xlqian <patrick.qian@canaltp.fr>
2016-06-16 16:22:35 +0200

    add missing file

---
+++ tests/osm2mimir_bano2mimir_test.rs
@@ -0,0 +1,59 @@
+// Copyright © 2016, Canal TP and/or its affiliates. All rights reserved.
+//
+// This file is part of Navitia,
+//     the software to build cool stuff with public transport.
+//
+// Hope you'll enjoy and contribute to this project,
+//     powered by Canal TP (www.canaltp.fr).
+// Help us simplify mobility and open public transport:
+//     a non ending quest to the responsive locomotion way of traveling!
+//
+// LICENCE: This program is free software; you can redistribute it
+// and/or modify it under the terms of the GNU Affero General Public
+// License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+//
+// Stay tuned using
+// twitter @navitia
+// IRC #navitia on freenode
+// https://groups.google.com/d/forum/navitia
+// www.navitia.io
+
+use std::process::Command;
+
+/// Simple call to a BANO load into ES base
+/// Checks that we are able to find one object (a specific address)
+pub fn osm2mimir_bano2mimir_test(es_wrapper: ::ElasticSearchWrapper) {
+    let osm2mimir = concat!(env!("OUT_DIR"), "/../../../osm2mimir");
+    let status = Command::new(osm2mimir)
+                     .args(&["--input=./tests/fixtures/rues_trois_communes.osm.pbf".into(),
+                             "--import-way".into(),
+                             "--import-admin".into(),
+                             "--level=8".into(),
+                             format!("--connection-string={}", es_wrapper.host())])
+                     .status()
+                     .unwrap();
+    assert!(status.success(), "`bano2mimir` failed {}", &status);
+
+    let bano2mimir = concat!(env!("OUT_DIR"), "/../../../bano2mimir");
+    info!("Launching {}", bano2mimir);
+    let status = Command::new(bano2mimir)
+                     .args(&["--input=./tests/fixtures/bano-trois_communes.csv".into(),
+                             format!("--connection-string={}", es_wrapper.host())])
+                     .status()
+                     .unwrap();
+    assert!(status.success(), "`bano2mimir` failed {}", &status);
+
+    es_wrapper.refresh();
+    
+	// TODO: more tests will be written here
+}
