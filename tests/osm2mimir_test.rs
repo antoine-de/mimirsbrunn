d47080ab2733257b302c2aeee1058ac8cee205d6
xlqian <patrick.qian@canaltp.fr>
2016-06-20 16:33:45 +0200

    fix test after rebase

--- tests/osm2mimir_test.rs
+++ tests/osm2mimir_test.rs
@@ -44,40 +44,40 @@ pub fn osm2mimir_sample_test(es_wrapper: ::ElasticSearchWrapper) {
                        &es_wrapper);
 
     // Test: Import of Admin
-    let city = es_wrapper.search("name:Livry-sur-Seine");
-    let nb_hits = city.lookup("hits.total").and_then(|v| v.as_u64()).unwrap_or(0);
+    let city = es_wrapper.search("label:Livry-sur-Seine");
+    let nb_hits = city.pointer("/hits/total").and_then(|v| v.as_u64()).unwrap_or(0);
     assert_eq!(nb_hits, 1);
     let city_type = city.pointer("/hits/hits/0/_type").and_then(|v| v.as_string()).unwrap_or("");
     assert_eq!(city_type, "admin");
     
     // Test: search for "Rue des Près"
-    let search = es_wrapper.search(r#"name:Rue des Près"#);
+    let search = es_wrapper.search("label:Rue des Près");
     // The first hit should be "Rue des Près"
-    let rue_name = search.pointer("/hits/hits/0/_source/name").and_then(|v| v.as_string()).unwrap_or("");
-    assert_eq!(rue_name, r#"Rue des Près"#);
+    let street_label = search.pointer("/hits/hits/0/_source/label").and_then(|v| v.as_string()).unwrap_or("");
+    assert_eq!(street_label, "Rue des Près");
     // And there should be only ONE "Rue des Près"
     let street_filter = |street: &Value| {
-    	         	let name = street.pointer("/_source/name").and_then(|n| n.as_string()).unwrap_or("");
-    	            name == r#"Rue des Près"#
+    	         	let label = street.pointer("/_source/label").and_then(|n| n.as_string()).unwrap_or("");
+    	            label == "Rue des Près"
     			};
-    let nb = es_wrapper.search_and_filter(r#"name:Rue des Près"#, street_filter).count();
+    let nb = es_wrapper.search_and_filter(r#"label:Rue des Près"#, street_filter).count();
     assert_eq!(nb, 1);
   
     // Test: Search for "Rue du Four à Chaux" in "Livry-sur-Seine"
     let street_filter = |street: &Value| {
-    	            let name = street.pointer("/_source/name").and_then(|n| n.as_string()).unwrap_or("");
-    	            let admin_name = street.pointer("/_source/administrative_regions/0/name").and_then(|n| n.as_string()).unwrap_or("");            
-    	            name == r#"Rue du Four à Chaux"# && admin_name == r#"Livry-sur-Seine"#
+    	            let label = street.pointer("/_source/label").and_then(|n| n.as_string()).unwrap_or("");
+    	            let admin_label = street.pointer("/_source/administrative_regions/0/label").and_then(|n| n.as_string()).unwrap_or("");            
+    	            label == "Rue du Four à Chaux" && admin_label == "Livry-sur-Seine"
     			};
-    let nb = es_wrapper.search_and_filter(r#"name:Rue du Four à Chaux"#, street_filter).count();
+    let nb = es_wrapper.search_and_filter("label:Rue du Four à Chaux", street_filter).count();
     assert_eq!(nb, 6);
     
-    //Test: Streets having the same name in different cities
+    //Test: Streets having the same label in different cities
     let street_filter = |street: &Value| {
-                        let name = street.pointer("/_source/name").and_then(|n| n.as_string()).unwrap_or("");
-                        let admin_name = street.pointer("/_source/administrative_regions/0/name").and_then(|n| n.as_string()).unwrap_or("");
-                        name == r#"Rue du Port"# && admin_name == r#"Melun"#
+                        let label = street.pointer("/_source/label").and_then(|n| n.as_string()).unwrap_or("");
+                        let admin_label = street.pointer("/_source/administrative_regions/0/label").and_then(|n| n.as_string()).unwrap_or("");
+                        label == r#"Rue du Port"# && admin_label == r#"Melun"#
     			};
-    let nb = es_wrapper.search_and_filter(r#"name:Rue du Port"#, street_filter).count();
+    let nb = es_wrapper.search_and_filter(r#"label:Rue du Port"#, street_filter).count();
     assert_eq!(nb, 1);
 }
