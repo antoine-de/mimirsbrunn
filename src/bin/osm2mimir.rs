ce2aca647ce0032855f8e18fdda9e6f230bf146f
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2016-06-28 11:58:06 +0200

    format label of street

--- src/bin/osm2mimir.rs
+++ src/bin/osm2mimir.rs
@@ -234,13 +234,12 @@ fn get_street_admin(admins_geofinder: &AdminGeoFinder,
     coord.map_or(vec![], |c| admins_geofinder.get(&c))
 }
 
-fn admin_by_level(admins: &AdminsVec, city_level: u32) -> Option<&Rc<mimir::Admin>> {
+fn format_label(admins: &AdminsVec, city_level: u32, name: &String) -> String {
     match admins.iter().position(|adm| adm.level == city_level) {
-        Some(idx) => Some(&admins[idx]),
-        None => None
+        Some(idx) => format!("{} ({})", name, admins[idx].label),
+        None => (*name).clone()
     }
 }
-
 fn streets(pbf: &mut OsmPbfReader, admins: &AdminsVec, city_level: u32) -> StreetsVec {
     let admins_geofinder = make_admin_geofinder(admins);
 
@@ -284,14 +283,10 @@ fn streets(pbf: &mut OsmPbfReader, admins: &AdminsVec, city_level: u32) -> Stree
                     way =<<obj.way();
                     way_name =<< way_name.or_else(|| way.tags.get("name"));
                     let admin = get_street_admin(admins_geofinder, objs_map, way);
-                    let label = match admin_by_level(&admin, city_level) {
-                        Some(adm) => format!("{} ({})", way_name, adm.label),
-                        None =>  way_name.to_string()
-                    };
                     ret ret(street_list.push(mimir::Street {
                                 id: way.id.to_string(),
                                 street_name: way_name.to_string(),
-                                label: label,
+                                label: format_label(&admin, city_level, way_name),
                                 weight: 1,
                                 administrative_regions: admin,
                     }))
@@ -346,14 +341,10 @@ fn streets(pbf: &mut OsmPbfReader, admins: &AdminsVec, city_level: u32) -> Stree
             way =<< obj.way();
             way_name =<< way.tags.get("name");
             let admins = get_street_admin(admins_geofinder, objs_map, way);
-            let label = match admin_by_level(&admins, city_level) {
-                Some(adm) => format!("{} ({})", way_name, adm.label),
-                None =>  way_name.to_string()
-            };
             ret ret(street_list.push(mimir::Street{
    	                    id: way.id.to_string(),
    	                    street_name: way_name.to_string(),
-   	                    label: label,
+   	                    label: format_label(&admins, city_level, way_name),
    	                    weight: 1,
    	                    administrative_regions: admins,
             }))
