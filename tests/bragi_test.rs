4377957c5eef4ed35a3ae1890412f297c5b3261a
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2016-11-21 15:09:46 +0100

    tests added

--- tests/bragi_test.rs
+++ tests/bragi_test.rs
@@ -63,6 +63,12 @@ pub fn bragi_tests(es_wrapper: ::ElasticSearchWrapper) {
             .unwrap()
     };
 
+    let bragi_params_validation = |q| {
+        iron_test::request::get(&format!("http://localhost:3000{}", q),
+                                iron::Headers::new(),
+                                &handler)
+    };
+
     let bragi_post_shape = |q, shape| {
         let mut header = iron::Headers::new();
         let mime: mime::Mime = "application/json".parse().unwrap();
@@ -96,7 +102,6 @@ pub fn bragi_tests(es_wrapper: ::ElasticSearchWrapper) {
             })
             .collect()
     };
-
     // Call status
     let resp = bragi_get("/status");
     let result_body = iron_test::response::extract_body_to_string(resp);
@@ -341,12 +346,27 @@ pub fn bragi_tests(es_wrapper: ::ElasticSearchWrapper) {
     assert!(get_postcodes(&geocodings).iter().all(|r| *r == "77000"));
 
     // search by zip code
-    let geocodings = get_results(bragi_get("/autocomplete?q=77000"));
+    let geocodings = get_results(bragi_get("/autocomplete?q=77000&size=15"));
     let types = get_types(&geocodings);
     assert_eq!(count_types(&types, "poi"), 2);
-    assert_eq!(count_types(&types, "city"), 2);
-    assert_eq!(count_types(&types, "street"), 6);
-    
+    assert_eq!(count_types(&types, "city"), 3);
+    assert_eq!(count_types(&types, "street"), 7);
+
+    // search by zip code and size is string type
+    let geocodings = bragi_params_validation("/autocomplete?q=77000&size=ABCD");
+    assert!(geocodings.is_err() , true);
+
+    // search by zip code and size < 0
+    let geocodings = bragi_params_validation("/autocomplete?q=77000&size=-1");
+    assert!(geocodings.is_err() , true);
+
+    // search by zip code and size and from
+    let all_20 = get_results(bragi_get("/autocomplete?q=77000&size=10&from=0"));
+    assert_eq!(all_20.len(), 10);
+
+    let all_20 = get_results(bragi_get("/autocomplete?q=77000&size=10&from=10"));
+    assert_eq!(all_20.len(), 2);
+
     // search poi: Poi is relation in osm data
     let geocodings = get_results(bragi_get("/autocomplete?q=Parking (Le Coudray-Montceaux)"));
     let types = get_types(&geocodings);
