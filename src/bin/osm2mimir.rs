5aba959661ae5280ea91fc17d836bb3f784dcf79
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2016-11-24 16:07:34 +0100

    rebase

--- src/bin/osm2mimir.rs
+++ src/bin/osm2mimir.rs
@@ -36,11 +36,11 @@ extern crate mimirsbrunn;
 extern crate rustc_serialize;
 
 use mimir::rubber::Rubber;
-use mimirsbrunn::osm_reader::admin::administrative_regions;
+use mimirsbrunn::osm_reader::admin::{administrative_regions,compute_admin_weight};
 use mimirsbrunn::osm_reader::poi::{PoiTypes, pois, default_amenity_types,
                                               default_leisure_types,
                                               compute_poi_weight};
-use mimirsbrunn::osm_reader::street::streets;
+use mimirsbrunn::osm_reader::street::{streets, compute_street_weight};
 use mimirsbrunn::osm_reader::parse_osm_pbf;
 use mimirsbrunn::admin_geofinder::AdminGeoFinder;
 
@@ -112,13 +112,13 @@ fn main() {
         info!("computing street weight");
         compute_street_weight(&mut streets, city_level);
 
-    if args.flag_import_way {
-        info!("importing streets into Mimir");
-        let nb_streets = rubber.index("way", &args.flag_dataset, streets.into_iter())
-            .unwrap();
-        info!("Nb of indexed street: {}", nb_streets);
+        if args.flag_import_way {
+            info!("importing streets into Mimir");
+            let nb_streets = rubber.index("way", &args.flag_dataset, streets.into_iter())
+                .unwrap();
+            info!("Nb of indexed street: {}", nb_streets);
+        }
     }
-
     let nb_admins = rubber.index("admin", &args.flag_dataset, admins.iter())
         .unwrap();
     info!("Nb of indexed admin: {}", nb_admins);
@@ -129,7 +129,7 @@ fn main() {
         poi_types.insert("leisure".to_string(), default_leisure_types());
 
         info!("Extracting pois from osm");
-        let mut pois = pois(&mut parsed_pbf, poi_types, &admins, city_level);
+        let mut pois = pois(&mut parsed_pbf, poi_types, &admins_geofinder, city_level);
 
         info!("computing poi weight");
         compute_poi_weight(&mut pois, city_level);
