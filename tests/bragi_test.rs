3a53e3757a7f7a96ae32b237450bda0bea40b693
patoche <patrick.degeilh@canaltp.fr>
2017-01-16 18:20:11 +0100

    bragi: update test

--- tests/bragi_test.rs
+++ tests/bragi_test.rs
@@ -114,14 +114,15 @@ pub fn bragi_tests(es_wrapper: ::ElasticSearchWrapper) {
     let resp = bragi_get("/autocomplete?q=15 Rue Hector Malot, (Paris)");
     let result_body = iron_test::response::extract_body_to_string(resp);
     let result = concat!(r#"{"type":"FeatureCollection","#,
-                r#""geocoding":{"version":"0.1.0","query":""},"#,
-                r#""features":[{"type":"Feature","geometry":{"coordinates":"#,
-                r#"[2.376379,48.846495],"type":"Point"},"#,
-                r#""properties":{"geocoding":{"id":"addr:2.376379;48.846495","#,
-                r#""type":"house","label":"15 Rue Hector Malot (Paris)","#,
-                r#""name":"15 Rue Hector Malot","housenumber":"15","#,
-                r#""street":"Rue Hector Malot","postcode":"75012","#,
-                r#""city":null,"citycode":null,"level":null,"administrative_regions":[]}}}]}"#);
+                         r#""geocoding":{"version":"0.1.0","query":""},"#,
+                         r#""features":[{"type":"Feature","geometry":{"coordinates":"#,
+                         r#"[2.376379,48.846495],"type":"Point"},"#,
+                         r#""properties":{"geocoding":{"id":"addr:2.376379;48.846495","#,
+                         r#""type":"house","label":"15 Rue Hector Malot (Paris)","#,
+                         r#""name":"15 Rue Hector Malot","housenumber":"15","#,
+                         r#""street":"Rue Hector Malot","postcode":"75012","#,
+                         r#""city":null,"city_code":null,"citycode":null,"level":null,"#,
+                         r#""administrative_regions":[]}}}]}"#);
     assert_eq!(result_body, result);
 
     // A(48.846431 2.376488)
@@ -147,14 +148,15 @@ pub fn bragi_tests(es_wrapper: ::ElasticSearchWrapper) {
 
     let result_body = iron_test::response::extract_body_to_string(resp);
     let result = concat!(r#"{"type":"FeatureCollection","#,
-                r#""geocoding":{"version":"0.1.0","query":""},"#,
-                r#""features":[{"type":"Feature","geometry":{"coordinates":"#,
-                r#"[2.376379,48.846495],"type":"Point"},"#,
-                r#""properties":{"geocoding":{"id":"addr:2.376379;48.846495","#,
-                r#""type":"house","label":"15 Rue Hector Malot (Paris)","#,
-                r#""name":"15 Rue Hector Malot","housenumber":"15","#,
-                r#""street":"Rue Hector Malot","postcode":"75012","#,
-                r#""city":null,"citycode":null,"level":null,"administrative_regions":[]}}}]}"#);
+                         r#""geocoding":{"version":"0.1.0","query":""},"#,
+                         r#""features":[{"type":"Feature","geometry":{"coordinates":"#,
+                         r#"[2.376379,48.846495],"type":"Point"},"#,
+                         r#""properties":{"geocoding":{"id":"addr:2.376379;48.846495","#,
+                         r#""type":"house","label":"15 Rue Hector Malot (Paris)","#,
+                         r#""name":"15 Rue Hector Malot","housenumber":"15","#,
+                         r#""street":"Rue Hector Malot","postcode":"75012","#,
+                         r#""city":null,"city_code":null,"citycode":null,"level":null,"#,
+                         r#""administrative_regions":[]}}}]}"#);
     assert_eq!(result_body, result);
 
     // Search with shape where house number out shape
