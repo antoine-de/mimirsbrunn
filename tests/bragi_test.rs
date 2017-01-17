18ea4978df66d5cd8436d09da90115324d802a8e
patoche <patrick.degeilh@canaltp.fr>
2017-01-17 12:45:34 +0100

    bragi: add test

--- tests/bragi_test.rs
+++ tests/bragi_test.rs
@@ -229,6 +229,8 @@ pub fn bragi_tests(es_wrapper: ::ElasticSearchWrapper) {
     let types = get_types(&all_20);
     let count = count_types(&types, "street");
     assert_eq!(count, 1);
+    let first_street = all_20.iter().find(|e| get_value(e, "type") == "street");
+    assert_eq!(get_value(first_street.unwrap(), "citycode"), "77255");
 
     let count = count_types(&types, "city");
     assert_eq!(count, 0);
@@ -246,6 +248,8 @@ pub fn bragi_tests(es_wrapper: ::ElasticSearchWrapper) {
 
     let count = count_types(&types, "city");
     assert_eq!(count, 1);
+    let first_city = all_20.iter().find(|e| get_value(e, "type") == "city");
+    assert_eq!(get_value(first_city.unwrap(), "citycode"), "77487");
 
     let count = count_types(&types, "house");
     assert_eq!(count, 0);
@@ -278,6 +282,8 @@ pub fn bragi_tests(es_wrapper: ::ElasticSearchWrapper) {
 
     let count = count_types(&types, "house");
     assert_eq!(count, 1);
+    let first_house = all_20.iter().find(|e| get_value(e, "type") == "house");
+    assert_eq!(get_value(first_house.unwrap(), "citycode"), "77255");
 
     // zip_code and name of addr
     let all_20 = get_results(bragi_get("/autocomplete?q=77288 Rue de la Reine Blanche"));
@@ -409,6 +415,8 @@ pub fn bragi_tests(es_wrapper: ::ElasticSearchWrapper) {
     let geocodings = get_results(bragi_get("/autocomplete?q=Parking (Le Coudray-Montceaux)"));
     let types = get_types(&geocodings);
     assert_eq!(count_types(&types, "poi"), 1);
+    let first_poi = geocodings.iter().find(|e| get_value(e, "type") == "poi");
+    assert_eq!(get_value(first_poi.unwrap(), "citycode"), "91179");
 
     // search poi: Poi is way in osm data
     let geocodings = get_results(bragi_get("/autocomplete?q=77000 Hôtel de Ville (Melun)"));
@@ -451,6 +459,7 @@ pub fn bragi_tests(es_wrapper: ::ElasticSearchWrapper) {
     assert_eq!(get_value(stop, "label"), "14 Juillet");
     assert_eq!(get_value(stop, "name"), "14 Juillet");
     assert_eq!(get_value(stop, "id"), "stop_area:SA:second_station");
+    assert_eq!(get_value(stop, "citycode"), "77487");
     // this stop area is in the boundary of the admin 'Vaux-le-Pénil',
     // it should have been associated to it
     assert_eq!(get_value(stop, "city"), "Vaux-le-Pénil");
