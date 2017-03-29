aeb3a5eb6ac48b97c22086289abb5a995c3201bf
kadhikari <krishna.adhikari@canaltp.fr>
2017-03-29 16:26:05 +0200

    bragi: error handling

--- libs/bragi/src/query.rs
+++ libs/bragi/src/query.rs
@@ -214,8 +214,15 @@ fn build_query(q: &str,
         .build()
 }
 
-fn is_existing_index(client: &mut rs_es::Client, index: &str) -> bool {
-    !index.is_empty() && client.open_index(&index).is_ok()
+fn is_existing_index(client: &mut rs_es::Client, index: &str) -> Result<bool, rs_es::error::EsError> {
+    if index.is_empty() {
+        return Ok(false);
+    }
+    match client.open_index(&index) {
+		Err(::rs_es::error::EsError::EsServerError(_)) => Err(rs_es::error::EsError::EsServerError("Index not found ".to_string())),
+		Err(_) => Err(rs_es::error::EsError::EsError("Elasticsearch Connection Error ".to_string())),
+		Ok(_) => Ok(true),
+    }	
 }
 
 fn get_indexes_by_type(a_type: &str) -> String {
@@ -229,45 +236,47 @@ fn get_indexes_by_type(a_type: &str) -> String {
     format!("munin_{}", doc_type)
 }
 
-fn make_indexes_impl<F: FnMut(&str) -> bool>(all_data: bool,
+fn make_indexes_impl<F: FnMut(&str) -> Result<bool, rs_es::error::EsError>> (all_data: bool,
                                              pt_dataset_index: &Option<String>,
                                              types: &Option<Vec<&str>>,
                                              mut is_existing_index: F)
-                                             -> Vec<String> {
+                                             -> Result<Vec<String>, rs_es::error::EsError> {
     if all_data {
-        return vec!["munin".to_string()];
+        return Ok(vec!["munin".to_string()]);
     }
 
     let mut result: Vec<String> = vec![];
-    let mut push = |result: &mut Vec<_>, i: &str| if is_existing_index(i) {
+    let mut push = |result: &mut Vec<_>, i: &str| -> Result<(), rs_es::error::EsError> {if try!(is_existing_index(i)) {
         result.push(i.into());
+    	}
+        Ok(())
     };
     if let Some(ref types) = *types {
         for type_ in types.iter().filter(|t| **t != "public_transport:stop_area") {
-            push(&mut result, &get_indexes_by_type(type_));
+            try!(push(&mut result, &get_indexes_by_type(type_)));
         }
         match *pt_dataset_index {
             Some(ref index) if types.contains(&"public_transport:stop_area") => {
-                push(&mut result, index)
+                try!(push(&mut result, index));
             }
             _ => (),
         }
     } else {
-        push(&mut result, &"munin_geo_data".to_string());
+        try!(push(&mut result, &"munin_geo_data".to_string()));
 
         if let Some(ref dataset) = *pt_dataset_index {
-            push(&mut result, &dataset.clone());
+            try!(push(&mut result, &dataset.clone()));
         }
     }
 
-    result
+    Ok(result)
 }
 
 fn make_indexes(all_data: bool,
                 pt_dataset_index: &Option<String>,
                 types: &Option<Vec<&str>>,
                 client: &mut rs_es::Client)
-                -> Vec<String> {
+                -> Result<Vec<String>, rs_es::error::EsError>  {
     make_indexes_impl(all_data,
                       pt_dataset_index,
                       types,
@@ -303,8 +312,8 @@ fn query(q: &str,
     let mut client = build_rs_client(&cnx.to_string());
 
     let pt_dataset_index = pt_dataset.map(|d| format!("munin_stop_{}", d));
-    let indexes = make_indexes(all_data, &pt_dataset_index, types, &mut client);
-
+    let indexes = try!(make_indexes(all_data, &pt_dataset_index, types, &mut client));
+    
     debug!("ES indexes: {:?}", indexes);
 
     if indexes.is_empty() {
@@ -340,7 +349,7 @@ pub fn features(pt_dataset: &Option<&str>,
     let mut client = build_rs_client(&cnx.to_string());
 
     let pt_dataset_index = pt_dataset.map(|d| format!("munin_stop_{}", d));
-    let indexes = make_indexes(all_data, &pt_dataset_index, &None, &mut client);
+    let indexes = try!(make_indexes(all_data, &pt_dataset_index, &None, &mut client));
 
     debug!("ES indexes: {:?}", indexes);
 
