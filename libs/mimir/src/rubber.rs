c3a8416aa6e12a82d881a690aa360462110f0723
Alexandre Jacquin <alexandre.jacquin@kisio.com>
2019-04-23 17:41:44 +0200

    rename do_req in check_response

--- libs/mimir/src/rubber.rs
+++ libs/mimir/src/rubber.rs
@@ -73,7 +73,7 @@ lazy_static::lazy_static! {
     .unwrap();
 }
 
-fn do_req(resp: reqwest::Response) -> Result<reqwest::Response, EsError> {
+fn check_response(resp: reqwest::Response) -> Result<reqwest::Response, EsError> {
     let mut resp = resp;
     let status = resp.status();
     match status {
@@ -259,21 +259,21 @@ impl Rubber {
         debug!("doing a get on {}", path);
         let url = self.es_client.full_url(path);
         let result = self.http_client.get(url).send()?;
-        do_req(result)
+        check_response(result)
     }
     fn put(&self, path: &str, body: &str) -> Result<reqwest::Response, EsError> {
         // Note: a bit duplicate on rs_es because some ES operations are not implemented
         debug!("doing a put on {} with {}", path, body);
         let url = self.es_client.full_url(path);
         let result = self.http_client.put(url).body(body.to_owned()).send()?;
-        do_req(result)
+        check_response(result)
     }
     fn post(&self, path: &str, body: &str) -> Result<reqwest::Response, EsError> {
         // Note: a bit duplicate on rs_es because some ES operations are not implemented
         debug!("doing a post on {} with {}", path, body);
         let url = self.es_client.full_url(path);
         let result = self.http_client.post(url).body(body.to_owned()).send()?;
-        do_req(result)
+        check_response(result)
     }
 
     pub fn make_index<T: MimirObject>(
