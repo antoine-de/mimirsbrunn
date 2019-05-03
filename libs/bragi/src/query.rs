c8171826bbcdcb6b536fa24307ff68348a743da3
Adrien Matissart <a.matissart@qwantresearch.com>
2019-05-03 18:51:43 +0200

    query: Use admin importance score

--- libs/bragi/src/query.rs
+++ libs/bragi/src/query.rs
@@ -37,6 +37,7 @@ use rs_es;
 use rs_es::error::EsError;
 use rs_es::operations::search::Source;
 use rs_es::query::compound::BoostMode;
+use rs_es::query::functions::Modifier;
 use rs_es::query::Query;
 use rs_es::units as rs_u;
 use serde;
@@ -163,13 +164,11 @@ fn build_query<'a>(
     let format_labels_field = |lang| format!("labels.{}", lang);
     let format_labels_prefix_field = |lang| format!("labels.{}.prefix", lang);
 
-    const I18N_FIELD_BOOST: f64 = 1.2;
     let build_multi_match =
         |default_field: &str, lang_field_formatter: &dyn Fn(&'a &'a str) -> String| {
             let boosted_i18n_fields = langs
                 .iter()
-                .map(lang_field_formatter)
-                .map(|f| format!("{}^{:.2}", f, I18N_FIELD_BOOST));
+                .map(lang_field_formatter);
             let fields: Vec<String> = iter::once(default_field.into())
                 .chain(boosted_i18n_fields)
                 .collect();
@@ -208,13 +207,28 @@ fn build_query<'a>(
         None => Query::build_function_score()
             .with_function(
                 Function::build_field_value_factor("weight")
-                    .with_factor(0.1)
+                    .with_factor(0.15)
                     .with_missing(0.)
                     .build(),
             )
             .with_boost_mode(BoostMode::Replace)
             .build(),
     };
+
+    let admin_importance_query = Query::build_function_score()
+        .with_query(
+            Query::build_term("_type", Admin::doc_type()).build()
+        )
+        .with_functions(vec![
+            Function::build_field_value_factor("weight")
+                .with_factor(1e6)
+                .with_modifier(Modifier::Log1p)
+                .with_missing(0.)
+                .build(),
+            Function::build_weight(0.03).build(),
+        ])
+        .with_boost_mode(BoostMode::Replace)
+        .build();
 
     // filter to handle house number
     // we either want:
@@ -274,7 +288,8 @@ fn build_query<'a>(
     }
 
     let mut query = Query::build_bool()
-        .with_must(vec![type_query, string_query, importance_query])
+        .with_must(vec![type_query, string_query])
+        .with_should(vec![importance_query, admin_importance_query])
         .with_filter(Query::build_bool().with_must(filters).build());
 
     if !zone_types.is_empty() {
