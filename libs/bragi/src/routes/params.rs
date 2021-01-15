0b90dc158f0156e0150ecd75239b8a72dfa5574b
crocme10 <matt@area403.org>
2021-01-15 15:43:08 +0100

    [Fixed] Lint related to range

--- libs/bragi/src/routes/params.rs
+++ libs/bragi/src/routes/params.rs
@@ -2,9 +2,9 @@ use crate::model::BragiError;
 use mimir::objects::Coord;
 
 pub fn make_coord(lon: f64, lat: f64) -> Result<Coord, BragiError> {
-    if lat < -90f64 || lat > 90f64 {
+    if !(-90f64..=90f64).contains(&lat) {
         Err(BragiError::InvalidParam("lat is not a valid latitude"))
-    } else if lon < -180f64 || lon > 180f64 {
+    } else if !(-180f64..=180f64).contains(&lon) {
         Err(BragiError::InvalidParam("lon is not a valid longitude"))
     } else {
         Ok(Coord::new(lon, lat))
