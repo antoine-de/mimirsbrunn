f5b7eff28bfc1d864b63874a1eeac700628d19fb
crocme10 <matt@area403.org>
2021-07-13 17:40:38 +0200

    Remove ci/gitlab directory

--- ci/gitlab/init.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-set -e
-set -x
-
-pip3 install docker-compose pipenv
-git clone -b master https://github.com/QwantResearch/docker_mimir.git
-mv ci/gitlab/*.yml docker_mimir/
-cd docker_mimir && pipenv install --system --deploy
