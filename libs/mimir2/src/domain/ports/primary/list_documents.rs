d7dc2dfcff2ee31e4ce0a8ff85ad42f2afd43f42
Rémi Dupré <r.dupre@qwant.com>
2021-09-02 14:21:44 +0200

    fix should_correctly_index_a_small_cosmogony_file

--- libs/mimir2/src/domain/ports/primary/list_documents.rs
+++ libs/mimir2/src/domain/ports/primary/list_documents.rs
@@ -3,12 +3,13 @@ use crate::domain::model::error::Error;
 use crate::domain::ports::secondary::list::{List, Parameters};
 use async_trait::async_trait;
 use futures::stream::{Stream, StreamExt};
+use std::pin::Pin;
+
+type PinnedStream<T> = Pin<Box<dyn Stream<Item = T> + Send + 'static>>;
 
 #[async_trait]
 pub trait ListDocuments<D> {
-    fn list_documents(
-        &self,
-    ) -> Result<Box<dyn Stream<Item = Result<D, Error>> + Send + 'static>, Error>;
+    fn list_documents(&self) -> Result<PinnedStream<Result<D, Error>>, Error>;
 }
 
 impl<D, T> ListDocuments<D> for T
@@ -17,9 +18,7 @@ where
     T: List,
     T::Doc: Into<serde_json::Value>,
 {
-    fn list_documents(
-        &self,
-    ) -> Result<Box<dyn Stream<Item = Result<D, Error>> + Send + 'static>, Error> {
+    fn list_documents(&self) -> Result<PinnedStream<Result<D, Error>>, Error> {
         let doc_type = D::static_doc_type().to_string();
         let raw_documents = self.list_documents(Parameters { doc_type })?;
 
@@ -27,6 +26,6 @@ where
             .map(|raw| raw.into())
             .map(|val| serde_json::from_value(val).map_err(Error::from_deserialization::<D>));
 
-        Ok(Box::new(documents))
+        Ok(Box::pin(documents))
     }
 }
