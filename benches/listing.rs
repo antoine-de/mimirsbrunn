f1c2f9525f4305d8680e2845e4195379db378a65
crocme10 <matt@area403.org>
2021-10-21 18:04:39 +0200

    set bench sample size 10

--- benches/listing.rs
+++ benches/listing.rs
@@ -69,8 +69,7 @@ fn bench(c: &mut Criterion) {
 
 criterion_group! {
     name = benches;
-    // config = Criterion::default().significance_level(0.1).sample_size(5);
-    config = Criterion::default();
+    config = Criterion::default().sample_size(10);
     targets = bench
 }
 criterion_main!(benches);
