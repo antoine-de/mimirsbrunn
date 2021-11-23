395631c4a9620116d0f35e610caba29c892c57e0
Rémi Dupré <r.dupre@qwant.com>
2021-11-23 07:32:11 +0100

    generate_index: introduce index creation handle for insertion & updates

--- libs/mimir/src/domain/ports/primary/generate_index.rs
+++ libs/mimir/src/domain/ports/primary/generate_index.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use crate::domain::model::configuration::ContainerConfig;
 use crate::domain::model::update::UpdateOperation;
 use crate::domain::model::{error::Error as ModelError, index::Index};
@@ -8,67 +10,43 @@ use futures::stream::Stream;
 use tracing::{info, info_span};
 use tracing_futures::Instrument;
 
-#[async_trait]
-pub trait GenerateIndex<'s> {
+#[async_trait(?Send)]
+pub trait GenerateIndex<'s>
+where
+    Self: Storage<'s> + Send + Sync + 'static,
+{
+    /// Generate an index and provide an handle over it.
+    async fn init_container<'a, D>(
+        &'a self,
+        config: &'a ContainerConfig,
+    ) -> Result<ContainerGenerator<'a, D, Self>, ModelError>
+    where
+        D: ContainerDocument + Send + Sync + 'static;
+
     /// Generate an index with provided stream of documents and publish it.
     async fn generate_index<D, S>(
-        &self,
-        config: &ContainerConfig,
+        &'s self,
+        config: &'s ContainerConfig,
         documents: S,
     ) -> Result<Index, ModelError>
     where
         D: ContainerDocument + Send + Sync + 'static,
         S: Stream<Item = D> + Send + Sync + 's;
-
-    /// Same as generate_index but also applies a stream of updates to
-    /// documents before publishing.
-    async fn generate_and_update_index<D, S, U>(
-        &self,
-        config: &ContainerConfig,
-        documents: S,
-        operations: U,
-    ) -> Result<Index, ModelError>
-    where
-        D: ContainerDocument + Send + Sync + 'static,
-        S: Stream<Item = D> + Send + Sync + 's,
-        U: Stream<Item = (String, UpdateOperation)> + Send + Sync + 's;
 }
 
-#[async_trait]
+#[async_trait(?Send)]
 impl<'s, T> GenerateIndex<'s> for T
 where
-    T: Storage<'s> + Send + Sync + 'static,
+    T: Storage<'s> + Send + Sync + Sized + 'static,
 {
-    async fn generate_index<D, S>(
-        &self,
-        config: &ContainerConfig,
-        documents: S,
-    ) -> Result<Index, ModelError>
+    #[tracing::instrument(skip(self, config))]
+    async fn init_container<'a, D>(
+        &'a self,
+        config: &'a ContainerConfig,
+    ) -> Result<ContainerGenerator<'a, D, Self>, ModelError>
     where
         D: ContainerDocument + Send + Sync + 'static,
-        S: Stream<Item = D> + Send + Sync + 's,
     {
-        self.generate_and_update_index(config, documents, futures::stream::empty())
-            .await
-    }
-
-    #[tracing::instrument(skip(self, config, documents, operations))]
-    async fn generate_and_update_index<D, S, U>(
-        &self,
-        config: &ContainerConfig,
-        documents: S,
-        operations: U,
-    ) -> Result<Index, ModelError>
-    where
-        D: ContainerDocument + Send + Sync + 'static,
-        S: Stream<Item = D> + Send + Sync + 's,
-        U: Stream<Item = (String, UpdateOperation)> + Send + Sync + 's,
-    {
-        // 1. We create the index
-        // 2. We insert the document stream in that newly created index
-        // 3. We update the documents with the input stream
-        // 4. We publish the index
-        // 5. We search for the newly created index to return it.
         let index = self
             .create_container(config)
             .instrument(info_span!("Create container"))
@@ -77,30 +55,97 @@ where
 
         info!("Created new index: {:?}", index);
 
+        Ok(ContainerGenerator {
+            storage: self,
+            config,
+            index,
+            _phantom: PhantomData::default(),
+        })
+    }
+
+    async fn generate_index<D, S>(
+        &'s self,
+        config: &'s ContainerConfig,
+        documents: S,
+    ) -> Result<Index, ModelError>
+    where
+        D: ContainerDocument + Send + Sync + 'static,
+        S: Stream<Item = D> + Send + Sync + 's,
+    {
+        self.init_container(config)
+            .await?
+            .insert_documents(documents)
+            .await?
+            .publish()
+            .await
+    }
+}
+
+/// Handle over an index which is beeing generated, it can be used to insert
+/// or update documents.  When all documents are ready, `.publish()` must be
+/// called to make the index available.
+#[must_use]
+pub struct ContainerGenerator<'a, D, T: ?Sized>
+where
+    D: ContainerDocument + Send + Sync + 'static,
+{
+    storage: &'a T,
+    config: &'a ContainerConfig,
+    index: Index,
+    _phantom: PhantomData<*const D>,
+}
+
+impl<'a, 's, D, T> ContainerGenerator<'a, D, T>
+where
+    D: ContainerDocument + Send + Sync + 'static,
+    T: Storage<'s> + Send + Sync,
+{
+    /// Insert new documents into the index
+    #[tracing::instrument(skip(self, documents))]
+    pub async fn insert_documents(
+        self,
+        documents: impl Stream<Item = D> + Send + Sync + 's,
+    ) -> Result<ContainerGenerator<'a, D, T>, ModelError> {
         let stats = self
-            .insert_documents(index.name.clone(), documents)
-            .instrument(info_span!("Insert documents"))
+            .storage
+            .insert_documents(self.index.name.clone(), documents)
             .await
             .map_err(|err| ModelError::DocumentStreamInsertion { source: err.into() })?;
 
-        info!("Index generation stats: {:?}", stats);
+        info!("Insertion stats: {:?}", stats);
+        Ok(self)
+    }
 
+    /// Update documents that have already been inserted
+    #[tracing::instrument(skip(self, updates))]
+    pub async fn update_documents(
+        self,
+        updates: impl Stream<Item = (String, UpdateOperation)> + Send + Sync + 's,
+    ) -> Result<ContainerGenerator<'a, D, T>, ModelError> {
         let stats = self
-            .update_documents(index.name.clone(), operations)
-            .instrument(info_span!("Update documents"))
+            .storage
+            .update_documents(self.index.name.clone(), updates)
             .await
             .map_err(|err| ModelError::DocumentStreamUpdate { source: err.into() })?;
 
-        info!("Index update stats: {:?}", stats);
+        info!("Update stats: {:?}", stats);
+        Ok(self)
+    }
 
-        self.publish_index(index.clone(), config.visibility)
-            .instrument(info_span!("Publish index"))
+    /// Publish the index, which consumes the handle
+    #[tracing::instrument(skip(self))]
+    pub async fn publish(self) -> Result<Index, ModelError> {
+        self.storage
+            .publish_index(self.index.clone(), self.config.visibility)
             .await
             .map_err(|err| ModelError::IndexPublication { source: err.into() })?;
 
-        self.find_container(index.name.clone())
+        self.storage
+            .find_container(self.index.name.clone())
             .await
             .map_err(|err| ModelError::DocumentStreamInsertion { source: err.into() })?
-            .ok_or(ModelError::ExpectedIndex { index: index.name })
+            .ok_or(ModelError::ExpectedIndex {
+                index: self.index.name,
+            })
     }
 }
