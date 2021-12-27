3044f33cacaffb3d73ac89acc0da31a72b4bfa3c
sdirollo <sebastien.dirollo@gmail.com>
2021-12-27 18:17:13 +0100

    default lang value defined with `serde`

--- libs/mimir/src/adapters/primary/bragi/handlers.rs
+++ libs/mimir/src/adapters/primary/bragi/handlers.rs
@@ -58,7 +58,7 @@ where
     let timeout = params.timeout;
     let es_indices_to_search_in =
         build_es_indices_to_search(&params.types, &params.pt_dataset, &params.poi_dataset);
-    let lang = params.lang.clone().unwrap_or_else(|| "fr".to_string());
+    let lang = params.lang.clone();
     let filters = filters::Filters::from((params, geometry));
     let dsl = dsl::build_query(&q, filters.clone(), lang, &settings);
 
@@ -117,11 +117,7 @@ where
     S::Document: Serialize + Into<serde_json::Value>,
 {
     let q = params.query.q.clone();
-    let lang = params
-        .query
-        .lang
-        .clone()
-        .unwrap_or_else(|| "fr".to_string());
+    let lang = params.query.lang.clone();
     let filters = filters::Filters::from((params.query, geometry));
     let dsl = dsl::build_query(&q, filters, lang, &settings);
 
