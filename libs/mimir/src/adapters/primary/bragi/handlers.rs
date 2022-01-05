2758281a2564b0e5719b7c67af4fc7ec5ddd365f
sdirollo <sebastien.dirollo@gmail.com>
2022-01-05 14:42:34 +0100

    use full_label field + add build weight function + update function score to ES7 format

--- libs/mimir/src/adapters/primary/bragi/handlers.rs
+++ libs/mimir/src/adapters/primary/bragi/handlers.rs
@@ -107,7 +107,9 @@ where
                     .map(|json| serde_json::from_value::<Place>(json.into()))
                     .collect();
                 match places {
-                    Ok(places) if places.is_empty() => {}
+                    Ok(places) if places.is_empty() => {
+                        println!("Empty !")
+                    }
                     Ok(places) => {
                         let features = places
                             .into_iter()
